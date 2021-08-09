3768c6f3ab39092e377fa66eb1484878322647fb
vmora60397@ufide.ac.cr <vmora60397>
2021-08-08 22:51:40 -0600

    add app updated

---
+++ DjangoWebProject1/DjangoWebProject1/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+from django.http import HttpRequest
+
+def index(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'index.html',
+       
+    )
+
+def agendar(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'agendar.html',
+        
+    )
+
+
+def alimentos(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'alimentos.html',
+        
+    )
+
+
+def buscar(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'buscar.html',
+        
+    )
+
+
+def contactos(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'contactos.html',
+        
+    )
+
+
+def crearexpediente(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'crear-expediente.html',
+        
+    )
+
+
+
+def verexpediente(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'ver-expediente.html',
+        
+    )
+
+def recetas(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'recetas.html',
+        
+    )
