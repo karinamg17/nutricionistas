35dd35c7196093f6dda1cb8d0379b52fa98e1419
Veronica <veromorav1987@gmail.com>
2021-10-14 00:19:08 -0600

    se migra de VS a VS Code

---
+++ ProyectoNutricional/settings.py
@@ -0,0 +1,122 @@
+"""
+Django settings for DjangoWebProject1 project.
+
+Based on by 'django-admin startproject' using Django 2.1.2.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/2.1/ref/settings/
+"""
+
+import os
+import posixpath
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'a1922ebd-7893-40d7-aa6a-e39c804f0440'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+STATICFILES_DIRS=['/Users/Veronica/Desktop/Universidad/ProyectoNutricional/ProyectoNutricional/Plantilla/static']
+
+# Application references
+# https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
+INSTALLED_APPS = [
+    # Add your apps here to enable them
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+# Middleware framework
+# https://docs.djangoproject.com/en/2.1/topics/http/middleware/
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'ProyectoNutricional.urls'
+
+# Template configuration
+# https://docs.djangoproject.com/en/2.1/topics/templates/
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ['/Users/Veronica/Desktop/Universidad/ProyectoNutricional/ProyectoNutricional/Plantilla'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'ProyectoNutricional.wsgi.application'
+
+# Database
+# https://docs.djangoproject.com/en/2.1/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : 'de6iseaflka80v',
+        'USER' : 'lylnlnksouqxtc',
+        'PASSWORD' : '2c3fd1bac949f41306ba4d8857fdda5975091b8bbb4f95d2dfa7b0e8b37ddd41',
+        'HOST' : 'ec2-34-233-105-94.compute-1.amazonaws.com',
+        'PORT' : '5432',
+        'OPTIONS': {
+            'options': '-c search_path=pln_schema'
+            }
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+# https://docs.djangoproject.com/en/2.1/topics/i18n/
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/2.1/howto/static-files/
+STATIC_URL = '/static/'
+STATIC_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['static']))
---
+++ ProyectoNutricional/urls.py
@@ -0,0 +1,40 @@
+"""
+DjangoWebProject1 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+# Uncomment next two lines to enable admin:
+from django.contrib import admin
+from django.urls import path
+
+from ProyectoNutricional import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('agendar/',views.agendar,name='agendar'),
+    path('alimentos/',views.alimentos,name='alimentos'),
+    path('buscar/',views.buscar,name='buscar'),
+    path('contactos/',views.contactos,name='contactos'),
+    path('crear-expediente/',views.crearexpediente,name='crear-expediente'),
+    path('ver-expediente/',views.verexpediente,name='ver-expediente'),
+    path('recetas/',views.recetas,name='recetas'),
+    path('pag-bloqueada/',views.pagbloqueada,name='pag-bloqueada'),
+    path('pag-olvido/',views.pagolvido,name='pag-olvido'),
+    path('pag-inicio/',views.paginicio,name='pag-inicio'),
+    path('pag-registro/',views.pagregistro,name='pag-registro'),
+    path('admin/', admin.site.urls),
+]
