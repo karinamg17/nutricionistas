ee130f112549d9e319f6c568e81c8423d0cf9b14
karinamg17 <81400581+karinamg17@users.noreply.github.com>
2021-11-02 13:07:01 -0600

    actualizacion BD

--- ProyectoNutricional/views.py
+++ ProyectoNutricional/views.py
@@ -38,7 +38,7 @@ def contactos(request):
     """Renders the about page."""
     "usuarios= Usuarios.objects.all()"
     cursor = connection.cursor()
-    cursor.execute("select usuarios.nombre,usuarios.primerapellido,usuarios.segundoapellido,usuarios.email,usuarios.telefono,usuarios.fecha_creacion,usuarios.estado , citas.fechacita from usuarios join citas on usuarios.idusuario=1")
+    cursor.execute("select usuarios.nombre,usuarios.primerapellido,usuarios.segundoapellido,usuarios.email,usuarios.telefono,usuarios.fecha_creacion,usuarios.estado , citas.fechacita from usuarios join citas on usuarios.idusuario=citas.idusuario")
     results= cursor.fetchall()
     return render(request,'contactos.html', {'uniontablas': results})
 
